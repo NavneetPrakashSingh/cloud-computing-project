3a67d52240326c8c600d200fcf346a67e57db3d0
NavneetPrakashSingh <navneet_prakash_singh@live.com>
2019-07-29 18:52:33 -0300

    resolving final bugs for master

---
+++ config/email.js
@@ -0,0 +1,12 @@
+//config/email.js
+module.exports.email = {
+    service: "Mailgun",
+    auth: {
+    user: "postmaster@xxxxxxxxxxxxx.mailgun.org", 
+    pass: "xxxxxxxxxxxxxxxxxxxxxx"
+    },
+    templateDir: "api/emailTemplates",
+    from: "info@mycompany.com",
+    testMode: false,
+    ssl: true
+   }
