b2376e3469a0c1ab5db8e3023f53ae6a58a7f39d
NavneetPrakashSingh <navneet_prakash_singh@live.com>
2019-07-29 23:47:42 -0300

    make it better

--- config/env/production.js
+++ config/env/production.js
@@ -371,7 +371,7 @@ module.exports = {
   *                                                                         *
   ***************************************************************************/
   custom: {
-    baseUrl: 'https://example.com',
+    baseUrl: 'https://cloud-computing-project-nodes.herokuapp.com',
     internalEmailAddress: 'support@example.com',
 
     // mailgunDomain: 'mg.example.com',
